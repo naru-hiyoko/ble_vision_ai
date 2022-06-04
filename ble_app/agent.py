d7af8121b46afaa1e9a6934789b7a71683a44b4b
Atsushi.Narusawa <atsushi.narusawa@access-company.com>
2022-06-04 11:45:26 +0900

    add agent.py

---
+++ ble_app/agent.py
@@ -0,0 +1,95 @@
+#!/usr/bin/python
+# SPDX-License-Identifier: LGPL-2.1-or-later
+
+from __future__ import absolute_import, print_function, unicode_literals
+
+import dbus
+import dbus.service
+import dbus.mainloop.glib
+
+BUS_NAME = 'org.bluez'
+AGENT_INTERFACE = 'org.bluez.Agent1'
+AGENT_PATH = "/test/agent"
+
+bus = None
+
+
+def ask(prompt):
+    try:
+        return raw_input(prompt)
+    except:
+        return input(prompt)
+
+
+def set_trusted(path):
+    props = dbus.Interface(bus.get_object("org.bluez", path), "org.freedesktop.DBus.Properties")
+    props.Set("org.bluez.Device1", "Trusted", True)
+
+
+def dev_connect(path):
+    dev = dbus.Interface(bus.get_object("org.bluez", path), "org.bluez.Device1")
+    dev.Connect()
+
+
+class Agent(dbus.service.Object):
+    path = '/test/agent'
+    exit_on_release = True
+
+    def __init__(self, bus):
+        dbus.service.Object.__init__(self, bus, self.path)
+
+    def set_exit_on_release(self, exit_on_release):
+        self.exit_on_release = exit_on_release
+
+    @dbus.service.method(AGENT_INTERFACE, in_signature="", out_signature="")
+    def Release(self):
+        print("Release")
+
+    @dbus.service.method(AGENT_INTERFACE, in_signature="os", out_signature="")
+    def AuthorizeService(self, device, uuid):
+        print("AuthorizeService (%s, %s)" % (device, uuid))
+        # authorize = ask("Authorize connection (yes/no): ")
+        # if (authorize == "yes"):
+        #     return
+        # raise RejectedException("Connection rejected by user")
+        return
+
+    @dbus.service.method(AGENT_INTERFACE, in_signature="o", out_signature="s")
+    def RequestPinCode(self, device):
+        print("RequestPinCode (%s)" % (device))
+        set_trusted(device)
+        return ask("Enter PIN Code: ")
+
+    @dbus.service.method(AGENT_INTERFACE, in_signature="o", out_signature="u")
+    def RequestPasskey(self, device):
+        print("RequestPasskey (%s)" % (device))
+        set_trusted(device)
+        passkey = ask("Enter passkey: ")
+        return dbus.UInt32(passkey)
+
+    @dbus.service.method(AGENT_INTERFACE, in_signature="ouq", out_signature="")
+    def DisplayPasskey(self, device, passkey, entered):
+        print("DisplayPasskey (%s, %06u entered %u)" % (device, passkey, entered))
+
+    @dbus.service.method(AGENT_INTERFACE, in_signature="os", out_signature="")
+    def DisplayPinCode(self, device, pincode):
+        print("DisplayPinCode (%s, %s)" % (device, pincode))
+
+    @dbus.service.method(AGENT_INTERFACE, in_signature="ou", out_signature="")
+    def RequestConfirmation(self, device, passkey):
+        print("RequestConfirmation (%s, %06d)" % (device, passkey))
+        # set_trusted(device)
+        # confirm = ask("Confirm passkey (yes/no): ")
+        # raise Rejected("Passkey doesn't match")
+        return
+
+    @dbus.service.method(AGENT_INTERFACE, in_signature="o", out_signature="")
+    def RequestAuthorization(self, device):
+        print("RequestAuthorization (%s)" % (device))
+        # set_trusted(device)
+        # dev_connect(device)
+        return
+
+    @dbus.service.method(AGENT_INTERFACE, in_signature="", out_signature="")
+    def Cancel(self):
+        print("Cancel")
